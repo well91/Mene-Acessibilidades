b8e333abed46c44110380b722ab7ec9b564ac0cb
wellington <welington.calligaris@hotmail.com>
2013-06-01 17:26:35 -0300

    Validacoes nos modelos: Caracteristica, detalheCaracteristica, Categoria, Inspecao

--- app/models/caracteristica.rb
+++ app/models/caracteristica.rb
@@ -1,5 +1,13 @@
+#encoding: utf-8
 class Caracteristica < ActiveRecord::Base
   belongs_to :categoria
   has_many :detalhes_inspecao
   attr_accessible :nome
+
+  validates :nome, :presence => true, :length => {:minimum => 3}
+  validate :categoria_inexistente
+
+  def categoria_inexistente
+	errors.add(:categoria_id, "Categoria inexistente") if Categoria.where(:id => categoria_id).empty? 	 
+  end
 end
