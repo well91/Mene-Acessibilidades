df92fa5093c9637da548d754720312bdf56c6016
vitorgroter <vitor023@gmail.com>
2013-06-05 09:04:26 -0300

    Adicionados os metodos padroes aos controllers

--- app/controllers/detalhes_inspecao_controller.rb
+++ app/controllers/detalhes_inspecao_controller.rb
@@ -1,3 +1,22 @@
 class DetalhesInspecaoController < ApplicationController
+  def index
+  end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
--- app/controllers/locais_controller.rb
+++ app/controllers/locais_controller.rb
@@ -1,3 +1,22 @@
 class LocaisController < ApplicationController
+  def index
+  end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
