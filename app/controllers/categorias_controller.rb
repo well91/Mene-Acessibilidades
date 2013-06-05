827e20066bb32909f8cfa1db3c4f3a28e9e9f88b
wellington <welington.calligaris@hotmail.com>
2013-06-05 09:29:03 -0300

    instalacao da gem simple_form

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,23 +1,30 @@
 class CategoriasController < ApplicationController
   def index
+    @categorias = Categoria.all
   end
 
   def show
+    @categoria = Categoria.find(:id)
   end
 
   def new
+    @categoria = Categoria.new
   end
 
   def create
+    @categoria = Categoria.new(params[:categoria])
   end
 
   def edit
+    @categoria = Categoria.find(:id)
   end
 
   def update
   end
 
   def delete
+    @categoria = Categoria.find(:id)
+    @categoria.destroy
   end
 end
 
